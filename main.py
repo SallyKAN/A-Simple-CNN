74074211718bbb629cedc5b5e9bdfa04358ffcf1
K <491251735@qq.com>
2018-10-22 23:30:04 +1100

    modify args

--- main.py
+++ main.py
@@ -242,7 +242,7 @@ if __name__ == '__main__':
                                      std=[0.229, 0.224, 0.225])
     train_transform = transforms.Compose(
         [
-            transforms.RandomResizedCrop (224),
+            transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             normalize,
@@ -250,7 +250,7 @@ if __name__ == '__main__':
     test_transform = transforms.Compose(
                 [
                     # resize,
-                    transforms.Resize(256),
+                    # transforms.Resize(256),
                     transforms.CenterCrop(224),
                     transforms.ToTensor(),
                     normalize,

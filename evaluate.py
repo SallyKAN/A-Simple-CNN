6ff4d3b4be6e5193835d17c3a832292fb5f136f9
K <491251735@qq.com>
2018-10-23 14:46:15 +1100

    modify args

--- evaluate.py
+++ evaluate.py
@@ -105,11 +105,10 @@ normalize = transforms.Normalize(
         std=[0.229, 0.224, 0.225],
     )
 
-
 test_transform = transforms.Compose(
                 [
-                    transforms.RandomResizedCrop(224),
-                    transforms.RandomHorizontalFlip(),
+                    transforms.Resize(256),
+                    transforms.CenterCrop(224),
                     transforms.ToTensor(),
                     normalize,
                  ])
